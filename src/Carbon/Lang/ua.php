dd55774af028f4278e680f80a692a9b7f415a430
maxboom <zmaxboomz@gmail.com>
2016-10-31 18:28:11 +0200

    update

--- src/Carbon/Lang/ua.php
+++ src/Carbon/Lang/ua.php
@@ -6,7 +6,7 @@
  * (c) Brian Nesbitt <brian@nesbot.com>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * file that was distributed with this source code. 
  */
 
 return array(
